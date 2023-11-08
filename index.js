de39abf67c6df66738b45783cb10de324c7ecf41
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2023-11-08 13:36:35 +0100

    intro to hbs

--- index.js
+++ index.js
@@ -5,8 +5,35 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 const port = process.env.SERVER_PORT || 3000;
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  let data = {
+    name: "Ironhacker",
+    lastName: "Rocking it!",
+    address: {
+      street: "Your heart",
+      number: 87,
+    },
+    cities: [
+      "Amsterdam",
+      "Barcelona",
+      "Berlin",
+      "Lisbon",
+      "Madrid",
+      "Mexico City",
+      "Miami",
+      "Paris",
+      "Sao Paulo",
+    ],
+  };
+
+  res.render("index", data);
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
